5f6a24338cca9f9a9d28bcde03edf387918cbf16
Lohanna <lhxsavage@gmail.com>
2017-09-26 13:42:24 -0300

    Tudo melhora com expressões regulares :)

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,14 +1,10 @@
-import string
+import re
 from collections import Counter
-
-exp = lambda stri : stri.split() if ' ' in stri else stri.replace(',','_').split('_')
-
 def word_count(word):
-	stri = ''
-	if word.find(string.punctuation) and ',' not in word:
-		for i,item in enumerate(word):
-			if word[i] not in string.punctuation:
-				stri += word[i]
-		return Counter(exp(stri.lower()))
-	else:
-		return Counter(exp(word.lower().replace('.','')))
+	if '_' in word:
+		word = word.replace('_',' ')
+	regex = re.sub('\W',' ',word.lower())
+	return Counter(regex.split())
+
+
+print(word_count('go Go GO Stop stop'))
