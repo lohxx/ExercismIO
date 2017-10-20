0ac1d329adb9fb228f7d090796b5e4b18dd69714
Lohanna <lhxsavage@gmail.com>
2017-10-19 23:42:32 -0200

    bob algorithm incompleto

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,8 +1,7 @@
-import re
 
 def hey(string):
 	regex = {'Sure.':string.rfind('?'),'Fine. Be that way!':string.isspace() or string == '','Whoa, chill out!':string.isupper() or string.endswith('!')}
-	expressions = list(regex.values())	
+	expressions = list(regex.values())
 	for index,exp in enumerate(regex):
 		if not expressions[index] < 1 :
 			return exp
