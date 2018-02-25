63ab533643781a160ca4e9435a0278ab8a34b785
Lohanna <lhxsavage@gmail.com>
2018-02-25 12:29:34 -0300

    isbn-verifier

---
+++ python/isbn-verifier/isbn_verifier.py
@@ -0,0 +1,24 @@
+import re
+import string
+
+
+def Validations(text):
+	alpha  = string.ascii_uppercase.replace('X','')
+	letter = re.findall(r'[a-zA-Z]',text)[0] if len(re.findall(r'[a-zA-Z]',text)) >= 1 else ''
+	if text == '' or text.count('X') > 1 or letter in list(alpha) or ('X' == letter and 'X' != text[len(text)-1]):
+		return False
+
+def verify(isbn):
+    total,digits = 0,[x for x in range(10,-1,-1)]
+    if Validations(isbn) == False:
+    	return False
+    for index,item in enumerate(re.findall(r'\w', isbn)):
+    	total +=  digits[index] * int(item) if item != 'X' else digits[index] * 10
+    return True if total % 11 == 0 else False
+
+print(verify('3-598-21507-A'))
+
+
+
+
+
