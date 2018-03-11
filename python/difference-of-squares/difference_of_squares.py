e754f56a4be1eb90bed946e2a7c2cab97fc83ca4
Lohanna <lhxsavage@gmail.com>
2018-03-11 12:24:32 -0300

    difference of squares

---
+++ python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,12 @@
+def square_of_sum(count):
+    return pow(sum([i for i in range(1,count+1)]),2)
+
+
+def sum_of_squares(count):
+    return sum([i**2 for i in range(1,count+1)])
+
+def difference(count):
+    return square_of_sum(count) - sum_of_squares(count)
+
+
+print(sum_of_squares(1))
